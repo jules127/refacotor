c9ffb623ec6bc7b33539d5a37b8ece0455f3cb0b
jules127 <zlei.sjack@gmail.com>
2025-11-22 00:34:53 -0500

    Task 1.1

--- src/main/java/theater/Statement.java
+++ src/main/java/theater/Statement.java
@@ -1,19 +1,26 @@
 package theater;
 
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 /**
- *
- * Initializer.
+ * Statement for an invoice.
+ * @null Required by the CSC207 Checkstyle configuration.
  */
 class Statement {
+
     private final Invoice invoice;
     private final Map<String, Play> plays;
     private final List<PerformanceData> performances;
 
-    public Statement(Invoice invoice, Map<String, Play> plays) {
+    /**
+     * Create a statement for an invoice and its plays.
+     *
+     * @param invoice the invoice
+     * @param plays   the plays for the performances in the invoice
+     */
+    Statement(Invoice invoice, Map<String, Play> plays) {
         this.invoice = invoice;
         this.plays = plays;
         this.performances = new ArrayList<>();
@@ -26,29 +33,41 @@ class Statement {
         return new PerformanceData(performance, plays.get(performance.getPlayID()));
     }
 
+    /**
+     * Get the customer name for this statement.
+     *
+     * @return the customer name
+     */
     public String getCustomer() {
         return invoice.getCustomer();
     }
 
+    /**
+     * Get the performances in this statement.
+     *
+     * @return the performances
+     */
     public List<PerformanceData> getPerformances() {
         return performances;
     }
 
     /**
-     * get.
-     * @return get.
+     * Get total amount for all performances.
+     *
+     * @return the total amount in cents
      */
     public int totalAmount() {
         int result = 0;
-        for (PerformanceData performanceData : performances ) {
-            result  += performanceData.amountFor();
+        for (PerformanceData performanceData : performances) {
+            result += performanceData.amountFor();
         }
         return result;
     }
 
     /**
-     * Get.
-     * @return Get.
+     * Get total volume credits for all performances.
+     *
+     * @return the total volume credits
      */
     public int volumeCredits() {
         int result = 0;
@@ -57,5 +76,5 @@ class Statement {
         }
         return result;
     }
-
 }
+
