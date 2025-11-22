a10488c85df3b9ff413c8076c23b10f7fc1b6fdd
jules127 <zlei.sjack@gmail.com>
2025-11-22 00:13:25 -0500

    Task 1.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -9,20 +9,50 @@ import java.util.Map;
  * This class generates a statement for a given invoice of performances.
  */
 public class StatementPrinter {
+    private final Invoice invoice;
+    private final Map<String, Play> plays;
     public final Statementdata statementdata;
     public StatementPrinter(Invoice invoice, Map<String, Play> plays) {
         this.statementdata = new Statementdata(invoice, plays);
+        this.invoice = invoice;
+        this.plays = plays;
     }
 
     public Statementdata getStatementdata() {
         return statementdata;
     }
 
+    public Map<String, Play> getPlays() {
+        return plays;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
     /**
      * Returns a formatted statement of the invoice associated with this printer.
      * @return the formatted statement
      * @throws RuntimeException if one of the play types is not known
      */
+    private Play getPlay(Performance performance) {
+        return getPlays().get(performance.getPlayID());
+    }
+    public int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : getInvoice().getPerformances() ) {
+            result  += getVolumeCredits(performance);
+        }
+        return result;
+    }
+
+    private int getVolumeCredits(Performance performance) {
+        int result = Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        // add extra credit for every five comedy attendees
+        if ("comedy".equals(performance.getAudience())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
+    }
 
     public String statement() {
         return renderPlainText();
